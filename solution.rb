bc8662d02350e09ae3b0dbf32b6919cc66c198dc
Reyes2777 <42840774+Reyes2777@users.noreply.github.com>
2018-12-19 10:32:01 -0500

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -1,2 +1,10 @@
 require "rubygems"
 require "sinatra"
+
+use Rack::Auth::Basic, "Restricted Area" do |permiso|
+    [permiso] == ["soy-un-token-secreto"] 
+end
+
+get '/' do
+    "You're welcome"
+end
