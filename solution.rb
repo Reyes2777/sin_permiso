07d5274520330c09e889cb20d4a9bd3cd99b9271
Reyes2777 <42840774+Reyes2777@users.noreply.github.com>
2018-12-19 11:12:18 -0500

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -1,5 +1,9 @@
-require "rubygems"
-require "sinatra"
+require 'sinatra'
 
 get '/' do
+      if request.env[HTTP_permiso]=="soy-un-token-secreto"
+        "Si lo logramos!"
+      else
+        "Sin Permiso"
+      end
 end
